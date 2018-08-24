ca16afb49ecaca7925d2204a3d37649df162cbe7
Ondřej Kotas <ondrej.kotas@solarwinds.com>
2018-08-24 17:18:25 +0200

    Kiosk - czech keyboard

---
+++ src/pirbanka.bankomat/scripts/jq-keyboard/jqk.layout.cs.js
@@ -0,0 +1,29 @@
+/* Czech */
+var jqKeyboard = jqKeyboard || {};
+
+jqKeyboard.layouts = [{
+        lang: "cs",
+        layout: [";|° + ě|Ě š|Š č|Č ř|Ř ž|Ž ý|Ý á|Á í|Í é|É ť|Ť ň|Ň ď|Ď <<backspace>>",
+                "<<tab>> q w e r t y u i o p ú|Ú )|( ",
+                "<<capslock>> a s d f g h j k l ů|Ů ó|Ó <<enter>>",
+                "<<shift>> z x c v b n m ,|? .|! -|_ <<shift>>",
+                "<<space>>"]
+    },
+    {
+        lang: "numeric",
+        layout: ["/ * - +",
+            "7 8 9",
+            "4 5 6",
+            "1 2 3",
+            "0 . ,",
+            "<<enter>>"]
+    },
+    {
+        lang: "en",
+        layout: ["`|~ 1|! 2|@ 3|# 4|$ 5|% 6|^ 7|& 8|* 9|( 0|) -|_ =|+ <<backspace>>",
+            "<<tab>> q w e r t y u i o p [|{ ]|} \\||",
+            "<<capslock>> a s d f g h j k l ;|: '|\" <<enter>>",
+            "<<shift>> z x c v b n m ,|< .|> /|? <<shift>>",
+            "<<space>>"]
+    }
+];
